011ba2362f1f4894ee6de386fc7d1c28b6155640
jonaemir33 <jonaemir33@gmail.com>
2022-02-03 20:34:57 -0700

    JohnPunkStore

---
+++ products/app.js
@@ -0,0 +1,47 @@
+const Buscar = document.getElementById('Buscar')
+
+const  Comprar1 = document.getElementById('Comprar1')
+const  Comprar2 = document.getElementById('Comprar2')
+const  Comprar3 = document.getElementById('Comprar3')
+const  Comprar4 = document.getElementById('Comprar4')
+const  Comprar5 = document.getElementById('Comprar5')
+const  Comprar6 = document.getElementById('Comprar6')
+const  Comprar7 = document.getElementById('Comprar7')
+const  Comprar8 = document.getElementById('Comprar8')
+
+
+Buscar.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar1.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar2.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar3.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar4.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar5.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar6.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar7.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
+
+Comprar8.addEventListener('click', () => {
+    alert('Haz sido hackeado, dame tu dinero!!! ahh te creas es curis')
+})
